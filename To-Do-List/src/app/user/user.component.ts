a83e9a21d9cffd11f9f0324a95466e34b5c56546
Joseph <joeprevost21@gmail.com>
2017-09-05 19:02:42 -0400

    Updated User components

--- To-Do-List/src/app/user/user.component.ts
+++ To-Do-List/src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  items:any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.items = [
+      {
+        prompt: 'write code',
+        isComplete: false
+      },
+      {
+        prompt: 'listen to music',
+        isComplete: false
+      }
+    ];
+  }
+
+  onClick(){
+    this.items.push({
+      prompt: 'New Items',
+      isComplete: false
+    });
+  }
+
+  addItem(item){
+    console.log(item);
+    this.items.unshift({
+      prompt: item.value,
+      isComplete: false
+    });
+    item.value = '';
+    return false;
+  }
+
+  lineThroughItem(item) {
+    item.isComplete = !item.isComplete;
+  }
+
+  deleteItem(item){
+    for(let i = 0; i <this.items.length; i++){
+      if(this.items[i] == item){
+        this.items.splice(i, 1);
+      }
+    }
   }
 
 }
